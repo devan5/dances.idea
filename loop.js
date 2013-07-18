a5a743c05142b17c4c9731f1f8f28b2f20c8c99a
devan5 <devan5.pan@gmail.com>
2013-07-19 01:24:54 +0800

    add loop.idea

---
+++ loop.js
@@ -0,0 +1,22 @@
+﻿/**
+ * @name 高阶函数-递归调用
+ * @Function loop
+ * @Describing fn 显示 false 则停止继续递归
+ */
+window.loop = (function(){
+    /**
+     * @param fn
+     * @param delay
+     */
+    return function loop(fn, delay){
+        var args = Array.prototype.slice.call(arguments, 0);
+
+        if(false !== fn.apply(null, args)){
+            setTimeout(function(){
+                loop.apply(null, args);
+                args = null;
+            }, delay);
+        }
+    }
+
+})();
