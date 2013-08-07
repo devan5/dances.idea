eae6ebb9dbbab82a0a0a475f95d6a0e8f3708c88
devan5 <devan5.pan@gmail.com>
2013-08-07 16:23:35 +0800

    去掉全局唯一性 增加多次调用判定

--- loop.js
+++ loop.js
@@ -1,9 +1,15 @@
 ﻿/**
  * @name 高阶函数-递归调用
  * @Function loop
- * @Describing fn 显示 false 则停止继续递归
+ * @Desc fn 显示 false 则停止继续递归
+ *
+ * @feature 多次多用返回的高阶函数, 逻辑判断是否执行
+ *
+ * @firstDate 2013.07.19
+ * @lastDate 2013.08.07
  */
-window.loop = (function(){
+
+window.loop = function(){
     /**
      * @param fn
      * @param delay
@@ -11,12 +17,21 @@ window.loop = (function(){
     return function loop(fn, delay){
         var args = Array.prototype.slice.call(arguments, 0);
 
+			if(loop._bExcuted){
+				return;
+			}
+
         if(false !== fn.apply(null, args)){
+				loop._bExcuted = true;
             setTimeout(function(){
+					loop._bExcuted = false;
                 loop.apply(null, args);
                 args = null;
             }, delay);
+
+			}else{
+				loop._bExcuted = false;
         }
     }
 
-})();
+};
